7686a446aabc51d9b22f963c3869c167d1b78c64
Adam English <englishadam@hotmail.co.uk>
2019-03-07 23:02:48 +0000

    Added tests for env file

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,30 @@
   <v-layout column justify-center align-center>
     <v-flex>
       <h1>Deepstream Server GUI</h1>
+      <p>{{ baseUrl }}</p>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      baseUrl: 'f'
+    }
+  },
+
+  asyncData(ctx) {
+    console.log('@', ctx)
+    return {
+      baseUrl: ctx.env.BASE_URL
+    }
+  },
+
+  methods: {
+    test() {
+      console.log('@')
+    }
+  }
 }
 </script>
