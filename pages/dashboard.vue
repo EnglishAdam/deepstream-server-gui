0ae83d114ee142f410477bd439b53de46c489b63
Adam English <englishadam@hotmail.co.uk>
2019-03-09 22:49:33 +0000

    Created Dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <p>Dashboard</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'dashboard'
+}
+</script>
