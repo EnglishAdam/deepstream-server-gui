f7fb8b6974c719bf3130a422814fff426840a7ed
Adam English <englishadam@hotmail.co.uk>
2019-03-09 20:30:33 +0000

    Updated linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,17 +8,21 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs'
-    // 'plugin:nuxt/recommended',
-    // 'plugin:prettier/recommended'
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended'
   ],
   plugins: [
-    // 'prettier'
+    'prettier'
   ],
   // add your custom rules here
   rules: {
     'no-console': 0,
     'nuxt/no-cjs-in-config': 0,
-    'import/order': 0
+    'import/order': 0,
+    'vue/attributes-order': 0,
+    'vue/max-attributes-per-line': 0,
+    'vue/html-indent': 0,
+    'vue/singleline-html-element-content-newline': 0
   }
 }
